bfb7ed6a59c3defc6f87ab320ff90217ff1f4b4b
05111942000029 <81474281+05111942000029@users.noreply.github.com>
2021-05-21 18:14:17 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,1 +1,19 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <syslog.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <dirent.h>
+#include <string.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <unistd.h>
 
